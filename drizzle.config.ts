80dbc9b66f8333f344a140bd19d927f09a541d53
HoangVanLuong2207 <luonghvpp03220@gmail.com>
2026-07-22 13:59:09 +0700

    fixbackend

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,10 +3,10 @@ import type { Config } from 'drizzle-kit';
 export default {
     schema: './src/db/schema.ts',
     out: './drizzle',
-    dialect: 'sqlite',
-    driver: 'turso',
+    dialect: 'turso',
     dbCredentials: {
         url: process.env.TURSO_DATABASE_URL!,
         authToken: process.env.TURSO_AUTH_TOKEN!,
     },
 } satisfies Config;
+
